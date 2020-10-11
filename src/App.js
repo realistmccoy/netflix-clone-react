76983f1333ba75331ee89eb9b75518411348ef00
Ramon Giron II <mbvault@MacBook-Pro.lan>
2020-10-10 21:18:29 -0400

    Row and axios inplace impending

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
-	return <div className='App'>
-    <h1> hey there
-      
-    </h1>
-  </div>;
+	return (
+		<div className='App'>
+			<h1> hey there</h1>
+			<Row
+				title='NETFLIX ORIGINALS'
+				fetchUrl={requests.fetchNetflexOriginals}
+			/>
+			<Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+			<Row title='' />
+			<Row title='' />
+			<Row title='' />
+			<Row title='' />
+			<Row title='' />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Row.js
@@ -0,0 +1,18 @@
+import React,{useState, useEffect} from 'react';
+
+function Row({ title }) {
+	const [Movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        
+    }, [])
+	return (
+		<div>
+			<h2>{title}</h2>
+
+			{/** */}
+		</div>
+	);
+}
+
+export default Row;
