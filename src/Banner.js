709597654c0076edd447e7934780a1a69b5fe0ae
Ramon Giron II <mbvault@MacBook-Pro.lan>
2020-10-25 20:42:22 -0400

    top banner css implemented

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner() {
 	const [movie, setMovie] = useState([]);
@@ -13,10 +14,14 @@ function Banner() {
 					Math.floor(Math.random() * request.data.results.length - 1)
 				],
 			);
-			return request; 
+			return request;
 		}
 		fetchData();
 	}, []);
+
+	function truncate(str, n) {
+		return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+	}
 
 	return (
 		<header
@@ -28,8 +33,18 @@ function Banner() {
 			}}
 		>
 			<div className='banner__contents'>
-				<h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+				<h1 className='banner__title'>
+					{movie?.title || movie?.name || movie?.original_name}
+				</h1>
+
+				<div class='banner__buttons'>
+					<button class='banner__button'>Play</button>
+					<button class='banner__button'>My List</button>
+
+					<h1 class='banner__description'>{truncate(movie?.overview)}</h1>
+				</div>
 			</div>
+			<div class="banner__fadeBottom"></div>
 		</header>
 	);
 }
